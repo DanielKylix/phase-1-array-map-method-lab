5a0210e1f9384472cd5d0b4b82cad00aa560c988
Daniel <daniel@kylix.online>
2022-08-19 09:35:40 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
+const newArray = [];
 const titleCased = () => {
-  return tutorials
-}
+  tutorials.map((tutorial) => {
+    const newTutorial = tutorial.charAt(0).toUpperCase() + tutorial.slice(1);
+    console.log(newTutorial);
+    newArray.push(newTutorial);
+  });
+  return newArray;
+};
+
+titleCased();
